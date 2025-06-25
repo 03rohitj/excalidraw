f72e9e9c55fbef2194a72f12c2f87545bdb2656a
Rohit Jangid <03rohitjangid@gmail.com>
2025-06-25 11:06:30 +0530

    created signup, signin and room endpoints, and wrote boilder code for now

---
+++ apps/http-backend/src/authMiddleware.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { json } from "zod/v4";
+import { JWT_SECRET } from "./config";
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    
+    try {
+        const token = req.headers.authorization;
+        if(!token){
+            throw new Error("UnAuthorized Access"); 
+        }
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+        if(!decoded)
+            throw new Error("Invalid token");
+
+        //@ts-ignore
+        req.userId = decoded.userId;        //###ToDO : Check how to structure the request object in express using TS - Do not use ts-ignore
+
+        next();
+
+    } catch (error) {
+        res.status(401).json({
+            message : error
+        });
+    }
+
+
+}
--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,57 @@
 import express from "express";
-
+import { z } from "zod";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { authMiddleware } from "./authMiddleware";
 const app = express();
+
+const signupZodSchema = z.object({
+    "username" : z.string().trim().min(6, "Username should be of minimum 6 letters"),
+    "password" : z.string().trim().min(6, "Password should be of minimum 6 letters")
+});
+
+app.post("/signup", async (req, res) =>{
+
+    try {
+        const result = signupZodSchema.safeParse(req.body);
+        if(!result.success){
+            throw new Error("Invalid Inputs : "+result.error);
+        }
+
+        const { username, password } = req.body;    
+
+        res.status(201).json({
+            "message" : "Successfully signedup with user : "+username
+        });
+    } catch (error) {
+        res.status(400).json({
+            "message" : error
+        });
+    }
+
+
+    
+});
+
+
+app.post("/signin", async (req, res) =>{
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        token
+    });
+});
+
+app.post("/room", authMiddleware , async (req, res) =>{
+    //db call
+
+    res.status(200).json({
+        roomId : 123
+    });
+});
 
 app.listen(3001);
