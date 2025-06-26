0c464d06a8c31e8e0d779d5c9b5f1075eeef1512
Rohit Jangid <03rohitjangid@gmail.com>
2025-06-26 08:27:27 +0530

    WebSocketServer init

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,25 @@
 import { WebSocketServer } from 'ws';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { JWT_SECRET } from './config';
 
 const wss = new WebSocketServer({ port: 8080 });
 // try tsx dependency for compilation
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+wss.on('connection', function connection(ws, request) {
+    ws.on('error', console.error);
+    const url = request.url;
+    if(!url){
+        return;
+    }
+
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get("token") || "";
+    const decoded = jwt.verify(token, JWT_SECRET);
+    if(!decoded || !(decoded as JwtPayload).userId){        //Infer decoded as JwtPayload
+        ws.close();
+        return;
+    }
+
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
