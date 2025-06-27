c498f343a2a63a50aa58aaeaf24483b71afaa3b7
Rohit Jangid <03rohitjangid@gmail.com>
2025-06-27 23:21:51 +0530

    Prisma migrate and BE routes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,48 +1,81 @@
 import express from "express";
-import { signupZodSchema } from "@repo/common/types";
+import { signinZodSchema, signupZodSchema } from "@repo/common/types";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
-
+import { prismaClient } from "@repo/db/client";
 import { authMiddleware } from "./authMiddleware";
 const app = express();
 
+app.use(express.json());            //Require to parse json into req object
+app.post("/signup", async(req, res) =>{
 
-
-app.post("/signup", async (req, res) =>{
-
+    console.log("------>>>/signup hit : "+req.body);
     try {
         const result = signupZodSchema.safeParse(req.body);
         if(!result.success){
             throw new Error("Invalid Inputs : "+result.error);
         }
 
-        const { username, password } = req.body;    
+        const { email, password, name } : {email:string, password:string, name:string} = result.data;    
+        const user = await prismaClient.user.create({
+            data: {
+                email: email,
+                password: password,
+                name: name
+            }
+        });
 
         res.status(201).json({
-            "userId"  : "123",
-            "message" : "Successfully signedup with user : "+username
+            "user Id"  : user.id,
+            "message" : "Successfully signedup with user : "+user.email
         });
+        return ;
     } catch (error) {
+        console.error("------->>>Signup error : "+error);
         res.status(400).json({
             "message" : error
         });
+        return;
     }
-
-
-    
 });
 
 
 app.post("/signin", async (req, res) =>{
+    console.log("------>>>/signin hit : ",req.body);
+    try {
+        const result = signinZodSchema.safeParse( req.body );
+        if(!result.success){
+            throw new Error("Invalid Input : "+result.error);
+        }
+        
+        const { email, password} = result.data;
+        
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email, password
+            }
+        });
 
-    const userId = 1;
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET);
+        if(!user){
+            throw new Error("Invalid Credentials");
+        }
 
-    res.json({
+        const token = jwt.sign({
+            userId : user.id
+        }, JWT_SECRET);
+
+    res.status(200).json({
+        "message" : user.name+" signed-in successfully",
         token
     });
+    return;
+
+    } catch (error) {
+        res.status(400).json({
+            "Message" : "Error : "+error
+        });
+        return;
+    }
 });
 
 app.post("/room", authMiddleware , async (req, res) =>{
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,13 +1,13 @@
 import {z} from "zod";
 
 export const signupZodSchema = z.object({
-    "username" : z.string().trim().min(6, "Username should be of minimum 6 letters"),
+    "email" : z.string().trim().min(6, "email should be of minimum 6 letters"),
     "password" : z.string().trim().min(6, "Password should be of minimum 6 letters"),
     "name"     : z.string()
 });
 
 export const signinZodSchema = z.object({
-    "username" : z.string().trim().min(6, "Username should be of minimum 6 letters"),
+    "email" : z.string().trim().min(6, "email should be of minimum 6 letters"),
     "password" : z.string().trim().min(6, "Password should be of minimum 6 letters")
 });
 
