7c912144caadbce5096471c426b5de3eef406b0b
Rohit Jangid <03rohitjangid@gmail.com>
2025-06-28 01:06:08 +0530

    WS room logic implemented

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,33 @@
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
 // try tsx dependency for compilation
 
+//User type to store list of users currently on the server
+interface User{
+  ws: WebSocket,      //ws object for each client
+  rooms: string[],    //What all rooms are they connected to
+  userId: string      //To uniquely identify the user
+}
+
+const users : User[] = [];    //This is usgly state management
+
+//Function returns userId if token is valid otherwise it returns null
+function checkUser(token: string) : string | null{
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(typeof decoded == "string"){
+      return null;
+    }
+
+    if(!decoded || !decoded.userId){        //Infer decoded as JwtPayload
+        return null;
+    }
+
+    return decoded.userId;
+}
 wss.on('connection', function connection(ws, request) {
     ws.on('error', console.error);
     const url = request.url;
@@ -14,22 +37,52 @@ wss.on('connection', function connection(ws, request) {
 
     const queryParams = new URLSearchParams(url.split("?")[1]);
     const token = queryParams.get("token") || "";
-    const decoded = jwt.verify(token, JWT_SECRET);
+    const userId = checkUser(token);
 
-    if(typeof decoded == "string"){
+    if(!userId){
       ws.close();
       return;
     }
 
-    if(!decoded || !decoded.userId){        //Infer decoded as JwtPayload
-        ws.close();
-        return;
-    }
+    //Add user to the list of connected users
+    users.push({
+      userId, 
+      rooms: [],
+      ws
+    });
 
 
   ws.on('message', function message(data) {
-    console.log('~~~~~server : received: %s', data);
+    const parsedData = JSON.parse(data as unknown as string);
+    //In data payload, type field should be passed to determine the request
+    if(parsedData.type === "join_room"){
+      const user = users.find(x => x.ws === ws);
+      //ToDo : verify roomId axists, verify user has permission to access room, etc etc
+      user?.rooms.push(parsedData.roomId);
+    }
+
+    if(parsedData.type === "leave_room"){
+      const user = users.find(x => x.ws === ws);
+      if(!user)
+        return
+
+      user.rooms = user.rooms.filter(x => x ===parsedData.room);
+    }
+
+    if(parsedData.type === "chat"){
+      const roomId = parsedData.roomId;
+      const message = parsedData.message;
+
+      users.forEach(user => {
+        if(user.rooms.includes(roomId)){
+          user.ws.send(JSON.stringify({     //We can send only string or binary data in websockets
+            type: "chat",
+            message : message,
+            roomId
+          }));
+        }
+      });
+    }
   });
 
-  ws.send('something');
 });
