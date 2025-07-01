7cbcfa70331901f264c539c2d3cbdc011ef50fd9
Rohit Jangid <03rohitjangid@gmail.com>
2025-07-01 10:02:13 +0530

    Restructure a bit

--- apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excalidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { initDraw } from "@/app/draw";
 import { useEffect, useRef } from "react";
 
 export default function Canvas(){
@@ -9,41 +10,12 @@ export default function Canvas(){
     useEffect(() => {
         if(canvasRef.current){      //If ref is not null
             const canvas = canvasRef.current;
-            const ctx = (canvas as HTMLCanvasElement).getContext('2d');
 
-            if(!ctx)
-                return;
-
-            let clicked = false;
-            let startX = 0, startY = 0;
-
-            canvas.addEventListener("mousedown", (e) => {
-                clicked = true;
-                startX = e.clientX;
-                startY = e.clientY;
-            });
-
-            canvas.addEventListener("mouseup", (e) => {
-                clicked = false;
-                console.log(e.clientX);
-                    console.log(e.clientY);
-                
-            });
-
-            canvas.addEventListener("mousemove", (e) => {
-                if(clicked){
-                    const width = e.clientX - startX;
-                    const height = e.clientY - startY;
-
-                    ctx.clearRect(0,0,canvas.height, canvas.width);
-                    ctx.strokeRect(startX, startY, width, height);
-                    
-                }
-            });
+            initDraw(canvas);
         }
     }, [canvasRef]);
     
     return <div>
-        <canvas ref={canvasRef} width={1000} height={1000}></canvas>
+        <canvas ref={canvasRef} width={"1800"} height={"900"}></canvas>
     </div>
 }
